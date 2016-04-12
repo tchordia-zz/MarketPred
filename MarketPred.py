42e224bb5ca80b46a6c0a29ba028462e16f100a0
tchordia <tchordia@gmail.com>
2016-04-12 13:23:34 -0400

    Added comments, cleaned up code

--- MarketPred.py
+++ MarketPred.py
@@ -1,20 +1,32 @@
 import pandas as pd
 import numpy as np
 
+# read the data -- I cleaned the original data by removing unncecessary rows etc. to
+# create anclean3.csv
+# in "data", data[1] refers to stock, while data[8] refers to tbills.
 data = pd.read_csv("anclean3.csv", skiprows = 1, header=None)
 data = data[[0, 1, 8]]
 data = data.iloc[8:47]
 data.dropna(inplace=True)
 data['val'] = data.apply(lambda x: 1 if x[1] > x[8] else 0, axis = 1)
 
-switchCost = .02;
+switchCost = .02
 
+
+# helper methods
 def succ(x,p):
     return p * x[1] + (1-p) * x[8]
 
 def fail(x,p):
     return p * x[8] + (1-p) * x[1]
 
+
+# Method 1 of calculating mean: iterate through every year,
+# and calculate the expected return, then average.
+
+# To compute Switching cost in that year,
+# subtract .02 from the probability of switching
+#and then subtract that
 def calcp(p):
     val = data.apply(lambda x: 1 if x[1] > x[8] else 0, axis = 1)
     suc = succ(data,p)
@@ -41,10 +53,18 @@ def stdev(p):
     pstock = p * pgood + q * (1 - pgood)
     pswitch = 2 * pstock * (1 - pstock)
 
+    # compute the mean as the probability of each of 4 permutations of the following events:
+    # 1) good yr vs bad yr,
+    #2) buy stock vs cash equiv
+
     mean = p * pgood * stock['mean'].iloc[1] + \
            q * pgood * tbill['mean'].iloc[1] + \
            p * (1 - pgood) * tbill['mean'].iloc[0] + \
            q * (1 - pgood) * stock['mean'].iloc[0]
+
+    adjMean = mean - pswitch * switchCost
+
+    #stdev is caculated from formula in Sharpe paper
 
     std1 = p * pgood * stock['std'].iloc[1]**2 + \
            q * pgood * tbill['std'].iloc[1]**2 +\
@@ -58,22 +78,15 @@ def stdev(p):
 
     std = (std1 + std2)**(.5)
 
-    adjMean = mean - pswitch * switchCost
+
     return pd.Series({'std': std, 'madjMean': adjMean, 'munadjMean':mean})
 
-#mean is calculated differently from standard deviation:
+
 ans = pd.DataFrame({'p' : range(50, 101)}) * .01
 ans[['Mean', 'Raw Mean']] = ans['p'].apply(calcp) * 100
+# This mean is calculated differently from previous mean,
+#both saved for sanity check
 ans[['Mean2','Raw Mean 2', 'Stdev']] = ans['p'].apply(stdev) * 100
-
 
 ans.to_csv("answer.csv")
 print ans
-# print data
-
-
-
-
-
-
-
